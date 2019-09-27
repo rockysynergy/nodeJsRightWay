c39a3a406ed2d2e5d39fb833ae859a29938a167d
Rocky Ou <rockysynergy@qq.com>
2019-09-27 15:29:47 +0800

    Finished chapter 5

---
+++ databases/rdf-to-json.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env Node
+
+const fs = require('fs');
+const parseRDF = require('./lib/parse-rdf.js');
+const rdf = fs.readFileSync(process.argv[2]);
+const book = parseRDF(rdf);
+console.log(JSON.stringify(book, null, '    '));
